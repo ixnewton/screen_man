b00880b4ede610123e34532344502f2b88ea9f9b
Ian Newton <i.newton@c-org.com>
2022-10-18 13:07:40 +0100

    commit screen_man.sh

--- screen-man.sh
+++ screen-man.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# screen_man
+# Desktop script to manage dual screen setup such as laptop with a usb-C/hdmi connected screen using xinput and xrandr
+
+# Identify your screen names using
+# xrandr --query | grep -w connected
+
+# Identify your input devices (touch input) using 
+# xinput --list
+
+# Identify your screen names using
+# xrandr --query | grep -w connected
+
+# Edit your touch screen device names as in grep 'TSTP MTouch' 
+
 # Deploy to /usr/local/bin/screen-man.sh with 755 permissions
 # Suggested custom key mappings
 #   
